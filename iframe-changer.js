2ccab6f1c84d6a2e9fe9008e7d5398a08832110b
Artem Kopytok <13ar.bass@gmail.com>
2021-09-21 19:29:20 +0300

    update: iframe changer works

--- iframe-changer.js
+++ iframe-changer.js
@@ -1,12 +1,16 @@
-
 let iframeItem = document.querySelector('#iframe-changer');
 let iframeCallButtons = document.querySelectorAll('.button--call-iframe');
 
 iframeCallButtons.forEach(button => {
     button.addEventListener('click', function() {
+        iframeCallButtons.forEach(el => {
+            el.classList.remove('button--call-iframe--active');
+        });
+        button.classList.add('button--call-iframe--active');
         let currentSrc = button.getAttribute('data-src');
         iframeItem.setAttribute('src', currentSrc);
     });
 });
 
 iframeItem.setAttribute('src', iframeCallButtons[0].getAttribute('data-src'));
+iframeCallButtons[0].classList.add('button--call-iframe--active');
